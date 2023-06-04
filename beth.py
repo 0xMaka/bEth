92984525fbc4572f7bd689945bfe38c0e3d5bb7a
Maka <makagucci@gmx.com>
2023-06-04 16:29:20 +0100

    [+] prototype clean

--- beth.py
+++ beth.py
@@ -1,17 +1,24 @@
 from os import getenv
 from dotenv import load_dotenv
-from discord import Client, Intents, Status, Activity, ActivityType, Game
+from discord import Intents, Game
 from asyncio import run, sleep
-load_dotenv()
-
-TOKEN = getenv('TOKEN')
-#PERMS = getenv('PERMS')
-
-#-----
 from aiohttp import ClientSession
-async def fetch_gas():
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = getenv('TOKEN')
+TIMEOUT = 5
+REST = 60
+
+intents = Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+#---
+
+async def fetch_prices():
   async with ClientSession() as session:
-    async with session.get('https://api.gasprice.io/v1/estimates',  timeout=5) as response:
+    async with session.get('https://api.gasprice.io/v1/estimates',  timeout=TIMEOUT) as response:
       if response.status == 200:
         r = (await response.json())['result']
         return [r['instant']['feeCap'], r['fast']['feeCap'], r['eco']['feeCap'], r['ethPrice']]
@@ -19,17 +26,15 @@ async def fetch_gas():
         raise Exception(f'Call failed. Return code: {request.status}. \n')
 #--
 
-from discord.ext import commands
-intents = Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
-
 @bot.command()
 async def beth(ctx):
   while True:
-    hi, mi, lo, usd = await fetch_gas()
+    hi, mi, lo, usd = await fetch_prices()
     name=f'$ETH: {usd}✨'
     await ctx.guild.me.edit(nick=name)
     game = Game(f'� {round(hi)} � {round(mi)} � {round(lo)}')
     await bot.change_presence(activity=game)
-    await sleep(120)
+    await sleep(REST)
+
+bot.run(TOKEN)
+#-
